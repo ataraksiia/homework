239156d65b64fff76634c4eb1945978603e6a2cb
Ataraksia <1509lisavi@gmail.com>
2024-10-07 23:10:21 +0300

    16.2

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ class BaseProduct(ABC):
 
     name: str
     description: str
-    price: float
+    # price: float
     quantity: int
 
     @abstractmethod
@@ -45,6 +45,89 @@ class Product(MixinLog, BaseProduct):
         self.__price = price
         self.quantity = quantity
         super().__init__()
+
+    @classmethod
+    def new_product(cls, dictionary: Any) -> Any:
+        """Класс-метод, который принимает на вход параметры товара в словаре
+        и возвращает созданный объект класса Product."""
+        name = dictionary.get("name")
+        description = dictionary.get("description")
+        price = dictionary.get("price")
+        quantity = dictionary.get("quantity")
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self) -> Any:
+        """Геттер для атрибута «цена»."""
+        return self.__price
+
+    @price.setter
+    def price(self, price: Any) -> Any:
+        """Сеттер для атрибута «цена»."""
+        if price >= 0:
+            self.__price = price
+        else:
+            raise ValueError("Цена не должна быть нулевая или отрицательная")
+
+    def __str__(self) -> str:
+        """Строковое отображение продукта."""
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт.\n"
+
+    def __add__(self, other: Any) -> Any:
+        """Сложение продуктов для получения полной стоимисти товаров на складе."""
+        if type(self) is type(other):
+            return self.quantity * self.__price + other.quantity * other.__price
+        else:
+            return TypeError
+
+
+class Smartphone(Product):
+    """Класс Smartphone - наследник класса Product."""
+
+    efficiency: float
+    model: str
+    memory: int
+    color: str
+
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        price: float,
+        quantity: int,
+        efficiency: float,
+        model: str,
+        memory: int,
+        color: str,
+    ):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    """Класс LawnGrass - наследник класса Product."""
+
+    country: str
+    germination_period: str
+    color: str
+
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        price: float,
+        quantity: int,
+        country: str,
+        germination_period: str,
+        color: str,
+    ):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
 
     @classmethod
     def new_product(cls, dictionary: Any) -> Any:
@@ -172,7 +255,7 @@ class Category:
         return f"{self.name}, количество продуктов: {total_quantity} шт."
 
 
-# if __name__ == "__main__":
+# if __name__ == '__main__':
 #     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 #     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
 #     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
@@ -192,13 +275,9 @@ class Category:
 #     print(product3.price)
 #     print(product3.quantity)
 #
-#     category1 = Category(
-#         "Смартфоны",
-#         "Смартфоны, как средство не только коммуникации,"
-#         " "
-#         "но и получения дополнительных функций для удобства жизни",
-#         [product1, product2, product3],
-#     )
+#     category1 = Category("Смартфоны",
+#           "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+#           [product1, product2, product3])
 #
 #     print(category1.name == "Смартфоны")
 #     print(category1.description)
@@ -206,12 +285,10 @@ class Category:
 #     print(category1.category_count)
 #     print(category1.product_count)
 #
-#     product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
-#     category2 = Category(
-#         "Телевизоры",
-#         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-#         [product4],
-#     )
+#     product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+#     category2 = Category("Телевизоры",
+#                  "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+#                   [product4])
 #
 #     print(category2.name)
 #     print(category2.description)
